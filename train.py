c0e1bab1e59b99b117b63e03d644c0e9860ae344
Jan Kaiser <jan.kaiser@desy.de>
2021-03-05 14:52:36 +0100

    Switched to new Pang-like model

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ env = gym.make("ARESExperimentalArea-Ocelot-v0")
 
 n_actions = env.action_space.shape[-1]
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-model = DDPG("CnnPolicy", env, action_noise=action_noise, buffer_size=20000, verbose=2)
+model = DDPG("MlpPolicy", env, action_noise=action_noise, buffer_size=20000, verbose=2)
 model.learn(total_timesteps=10000, log_interval=1)
 
 model.save("model_maxwell")
