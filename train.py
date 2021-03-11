67cce5398a10e6ef245e469462cfb76ca914cd40
Jan Kaiser <jan.kaiser@desy.de>
2021-03-11 14:10:53 +0100

    Moved to JOSS and added normalisation

--- train.py
+++ train.py
@@ -5,11 +5,13 @@ from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import OrnsteinUhlenbeckActionNoise
 
 
-env = gym.make("ARESExperimentalArea-Ocelot-v0")
+env = gym.make("ARESExperimentalArea-JOSS-v0")
+env = accelerator_environments.wrappers.NormalizeAction(env)
+env = accelerator_environments.wrappers.NormalizeObservation(env)
 
 n_actions = env.action_space.shape[-1]
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 model = DDPG("MlpPolicy", env, action_noise=action_noise, buffer_size=20000, verbose=2)
-model.learn(total_timesteps=10000, log_interval=1)
+model.learn(total_timesteps=800, log_interval=1)
 
-model.save("model_maxwell")
+model.save("model_pang")
