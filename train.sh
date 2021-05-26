1e3c99346d3a45bc250cf9d46a448ec6ccc2434d
Jan Kaiser <jan.kaiser@desy.de>
2021-05-26 10:21:24 +0200

    Moved to MAXGPU

--- train.sh
+++ train.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-#SBATCH --partition=maxwell
+#SBATCH --partition=maxgpu
 #SBATCH --job-name sigma-scale
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
+#SBATCH --constraint=V100|P100
 #SBATCH --mail-type ALL
 
 source /etc/profile.d/modules.sh
