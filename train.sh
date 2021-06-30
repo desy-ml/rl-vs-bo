08b364568f23c5fb791de06e895095a5f7fde054
Jan Kaiser <jan.kaiser@desy.de>
2021-06-30 15:09:07 +0200

    Training to verfify moved environment

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --partition=maxgpu
-#SBATCH --job-name upstream-screen-lr-lunarmodel
+#SBATCH --job-name move-verfication
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --constraint=V100|P100
