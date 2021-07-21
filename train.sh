538b462bf1f08fb6c0952888d363917de67361bb
Jan Kaiser <jan.kaiser@desy.de>
2021-07-21 16:17:12 +0200

    Training setup

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --partition=maxgpu
-#SBATCH --job-name flattened-goalenv-v3
+#SBATCH --job-name doubled-negatives
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --constraint=V100|P100
