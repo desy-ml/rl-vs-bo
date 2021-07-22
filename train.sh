502161ef1941b7d0c87925bee669b703b6756d5f
Jan Kaiser <jan.kaiser@desy.de>
2021-07-22 10:29:48 +0200

    Setup training

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --partition=maxgpu
-#SBATCH --job-name log-objective-doubled-negatives
+#SBATCH --job-name boni-and-done
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --constraint=V100|P100
