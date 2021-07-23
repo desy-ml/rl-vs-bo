e54df26676c1e5c6f52e3925429ceed5976a519d
Jan Kaiser <jan.kaiser@desy.de>
2021-07-23 15:12:27 +0200

    Setup training

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --partition=maxgpu
-#SBATCH --job-name boni-and-done
+#SBATCH --job-name cheetah-early-done
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
 #SBATCH --constraint=V100|P100
