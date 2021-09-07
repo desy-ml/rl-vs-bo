736499f5cbf970d6aa31a8855d5f1600f309a264
Jan Kaiser <jan.kaiser@desy.de>
2021-09-07 17:24:52 +0200

    Setup oneshot maxwell training

---
+++ oneshot.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --partition=maxgpu
+#SBATCH --job-name oneshot
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --constraint=V100|P100
+#SBATCH --mail-type ALL
+
+source /etc/profile.d/modules.sh
+
+source ~/.bashrc
+conda activate rl37
+
+cd /beegfs/desy/user/kaiserja/ares-ea-rl
+python3 oneshot0script.py
+
+exit
