827900d469064229be2625a70c5fbf99a4bdcd43
Jan Kaiser <jan.kaiser@desy.de>
2021-09-15 16:13:12 +0200

    Moved one step training to CPU node

--- train_onestep.sh
+++ train_onestep.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#SBATCH --partition=maxgpu
+# #SBATCH --partition=maxgpu
+#SBATCH --partition=maxwell
 #SBATCH --job-name sb3-ppo
 #SBATCH --time=1-00:00:00
 #SBATCH --nodes=1
-#SBATCH --constraint=V100|P100
+# #SBATCH --constraint=V100|P100
 #SBATCH --mail-type ALL
 
 source /etc/profile.d/modules.sh
