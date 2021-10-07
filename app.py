a9bba3a9770300676696fba3e631ebce91231ed5
Jan Kaiser <jan.kaiser@desy.de>
2021-10-07 19:54:55 +0200

    Factor out auxiliary channels and bugfixes

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ import pyqtgraph as pg
 from stable_baselines3 import PPO, TD3
 import torch
 
+from config import auxiliary_channels
 from environments import machine, utils
 from environments.onestep_ppo import ARESEAOneStep
 from environments.sequential import ARESEASequential
@@ -257,77 +258,6 @@ class AgentThread(qtc.QThread):
 
     step_permission_event = Event()
 
-    auxiliary_channels = [
-        "SINBAD.RF/LLRF.CONTROLLER/VS.AR.LI.RSB.G.1/AMPL.SAMPLE",       # Gun amplitude
-        "SINBAD.RF/LLRF.CONTROLLER/VS.AR.LI.RSB.G.1/PHASE.SAMPLE",      # Gun phase
-        "SINBAD.RF/LLRF.CONTROLLER/PROBE.AR.LI.RSB.G.1/POWER.SAMPLE",   # Gun power
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMSOG1+-/FIELD.RBV",               # Solenoid field at center
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMSOG1+-/CURRENT.RBV",             # Solenoid current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMSOG1+-/MOMENTUM.SP",             # Solenoid beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG1/KICK.RBV",                  # HCor (ARLIMCHG1) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG1/CURRENT.RBV",               # HCor (ARLIMCHG1) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG1/MOMENTUM.SP",               # HCor (ARLIMCHG1) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG1/KICK.RBV",                  # VCor (ARLIMCVG1) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG1/CURRENT.RBV",               # VCor (ARLIMCVG1) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG1/MOMENTUM.SP",               # VCor (ARLIMCVG1) beam momentum setting
-        "SINBAD.UTIL/INJ.LASER.MOTOR/MOTOR1.MBDEV1/POS",                # Laser attenuation
-        "SINBAD.LASER/SINBADCPULASER1.SETTINGS/SINBAD_aperture_pos/SETTINGS.CURRENT",   # Laser aperture
-        "SINBAD.DIAG/SCREEN.MOTOR/MOTOR4.MBDEV4/POS",                   # Collimator x position
-        "SINBAD.DIAG/COLLIMATOR.ML/AR.LI.SLH.G.1/POS",                  # Collimator y position
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG2/KICK.RBV",                  # HCor (ARLIMCHG2) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG2/CURRENT.RBV",               # HCor (ARLIMCHG2) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG2/MOMENTUM.SP",               # HCor (ARLIMCHG2) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG2/KICK.RBV",                  # VCor (ARLIMCVG2) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG2/CURRENT.RBV",               # VCor (ARLIMCVG2) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG2/MOMENTUM.SP",               # VCor (ARLIMCVG2) beam momentum setting
-        "SINBAD.DIAG/DARKC_MON/AR.LI.BCM.G.1/CHARGE.CALC",              # Dark current charge (?)
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMSOL1++/CURRENT.RBV",             # TWS1 current
-        "SINBAD.RF/LLRF.CONTROLLER/CTRL.AR.LI.RSB.L.1/SP.PHASE",        # TWS1 phase
-        "SINBAD.RF/LLRF.CONTROLLER/FORWARD.AR.LI.RSB.L.1/POWER.SAMPLE", # TWS1 power
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG3/KICK.RBV",                  # HCor (ARLIMCHG3) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG3/CURRENT.RBV",               # HCor (ARLIMCHG3) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG3/MOMENTUM.SP",               # HCor (ARLIMCHG3) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG3/KICK.RBV",                  # VCor (ARLIMCVG3) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG3/CURRENT.RBV",               # VCor (ARLIMCVG3) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG3/MOMENTUM.SP",               # VCor (ARLIMCVG3) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHM1/KICK.RBV",                  # HCor (ARLIMCHM1) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHM1/CURRENT.RBV",               # HCor (ARLIMCHM1) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHM1/MOMENTUM.SP",               # HCor (ARLIMCHM1) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVM1/KICK.RBV",                  # VCor (ARLIMCVM1) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVM1/CURRENT.RBV",               # VCor (ARLIMCVM1) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVM1/MOMENTUM.SP",               # VCor (ARLIMCVM1) beam momentum setting
-        "SINBAD.RF/LLRF.CONTROLLER/CTRL.AR.LI.RSB.L.2/SP.AMPL",         # TWS2 current
-        "SINBAD.RF/LLRF.CONTROLLER/CTRL.AR.LI.RSB.L.2/SP.PHASE",        # TWS2 phase
-        "SINBAD.RF/LLRF.CONTROLLER/FORWARD.AR.LI.RSB.L.2/POWER.SAMPLE", # TWS2 power
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG4/KICK.RBV",                  # HCor (ARLIMCHG4) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG4/CURRENT.RBV",               # HCor (ARLIMCHG4) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCHG4/MOMENTUM.SP",               # HCor (ARLIMCHG4) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG4/KICK.RBV",                  # VCor (ARLIMCVG4) kick
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG4/CURRENT.RBV",               # VCor (ARLIMCVG4) current
-        "SINBAD.MAGNETS/MAGNET.ML/ARLIMCVG4/MOMENTUM.SP",               # VCor (ARLIMCVG4) beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM1/STRENGTH.RBV",              # Q1 K1
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM1/CURRENT.RBV",               # Q1 current
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM1/MOMENTUM.SP",               # Q1 beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM2/STRENGTH.RBV",              # Q2 K1
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM2/CURRENT.RBV",               # Q2 current
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM2/MOMENTUM.SP",               # Q2 beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCVM1/KICK_MRAD.RBV",             # CV kick (mrad)
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCVM1/CURRENT.RBV",               # CV current
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCVM1/MOMENTUM.SP",               # CV beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM3/STRENGTH.RBV",              # Q3 K1
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM3/CURRENT.RBV",               # Q3 current
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMQZM3/MOMENTUM.SP",               # Q3 beam momentum setting
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCHM1/KICK_MRAD.RBV",             # CH kick (mrad)
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCHM1/CURRENT.RBV",               # CH current
-        "SINBAD.MAGNETS/MAGNET.ML/AREAMCHM1/MOMENTUM.SP",               # CH beam momentum setting
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/BINNINGHORIZONTAL",           # Screen horizontal binning 
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/BINNINGVERTICAL",             # Screen vertical binning
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/WIDTH",                       # Screen image width (pixel)
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/HEIGHT",                      # Screen image height (pixel)
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/GAINRAW",                     # Screen camera gain
-        "SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/GAINAUTO",                    # Screen camera auto gain setting
-    ]
-
     def __init__(self, model_name, desired_goal, target_delta, experiment_name):
         super().__init__()
 
@@ -382,7 +312,7 @@ class AgentThread(qtc.QThread):
             "action": [],
             "time": [time.time()]
         }
-        self.log_channels(log, self.auxiliary_channels)
+        self.log_channels(log, auxiliary_channels)
         self.agent_screen_updated.emit(self.env.screen_data)
         self.achieved_updated.emit(*self.env.unwrapped.observation["achieved_goal"])
         while not done:
@@ -400,7 +330,7 @@ class AgentThread(qtc.QThread):
             log["screen_data"].append(self.env.unwrapped.screen_data)
             log["observation"].append(self.env.unwrapped.observation)
             log["action"].append(self.env.unwrapped.action2accelerator(action))
-            self.log_channels(log, self.auxiliary_channels)
+            self.log_channels(log, auxiliary_channels)
             log["time"].append(time.time())
 
             self.actuators_updated.emit(*self.env.accelerator.actuators)
@@ -457,7 +387,7 @@ class AgentThread(qtc.QThread):
             "initial_actuators": self.env.accelerator.actuators,
             "time": [time.time()]
         }
-        self.log_channels(log, self.auxiliary_channels)
+        self.log_channels(log, auxiliary_channels)
 
         self.agent_screen_updated.emit(self.env._screen_data)
         self.achieved_updated.emit(*self.env.achieved)
@@ -487,7 +417,7 @@ class AgentThread(qtc.QThread):
             log["final_screen_data"] = self.env._screen_data
             log["final_achieved"] = self.env.achieved
             log["final_actuators"] = self.env.accelerator.actuators
-            self.log_channels(log, self.auxiliary_channels)
+            self.log_channels(log, auxiliary_channels)
             log["time"].append(time.time())
 
             self.agent_screen_updated.emit(self.env._screen_data)
@@ -529,7 +459,7 @@ class AgentThread(qtc.QThread):
             "initial_actuators": self.env.accelerator.actuators,
             "time": [time.time()]
         }
-        self.log_channels(log, self.auxiliary_channels)
+        self.log_channels(log, auxiliary_channels)
 
         self.agent_screen_updated.emit(self.env._screen_data)
         self.achieved_updated.emit(*self.env.achieved)
@@ -546,7 +476,7 @@ class AgentThread(qtc.QThread):
             log["final_screen_data"] = self.env._screen_data
             log["final_achieved"] = self.env.achieved
             log["final_actuators"] = self.env.accelerator.actuators
-            self.log_channels(log, self.auxiliary_channels)
+            self.log_channels(log, auxiliary_channels)
             log["time"].append(time.time())
 
             self.agent_screen_updated.emit(self.env._screen_data)
@@ -591,7 +521,7 @@ class AgentThread(qtc.QThread):
             "actuators": [self.env.actuators],
             "time": [time.time()]
         }
-        self.log_channels(log, self.auxiliary_channels)
+        self.log_channels(log, auxiliary_channels)
 
         self.agent_screen_updated.emit(self.env._screen_data)
         self.achieved_updated.emit(*self.env.achieved)
@@ -619,7 +549,7 @@ class AgentThread(qtc.QThread):
             log["screen_data"].append(self.env._screen_data)
             log["achieved"].append(self.env.achieved)
             log["actuators"].append(self.env.actuators)
-            self.log_channels(log, self.auxiliary_channels)
+            self.log_channels(log, auxiliary_channels)
             log["time"].append(time.time())
 
             self.agent_screen_updated.emit(self.env._screen_data)
@@ -659,7 +589,7 @@ class AgentThread(qtc.QThread):
             log["final_screen_data"] = self.env._screen_data
             log["final_achieved"] = self.env.achieved
             log["final_actuators"] = self.env.actuators
-            self.log_channels(log, self.auxiliary_channels)
+            self.log_channels(log, auxiliary_channels)
             log["time"].append(time.time())
             # log["optimizer.space"] = optimizer.space
 
