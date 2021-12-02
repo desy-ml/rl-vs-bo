f3e2d171b364cbdd0d2160feb62afd41684343d4
Local aresoper account <aresoper@bkrlxsicon05.desy.de>
2021-12-02 09:55:29 +0100

    Shell script for easy starting of the machine evaluation by operators

---
+++ evaluate_machine_agents.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Activating conda environment"
+conda activate /home/kaiserja/.conda/envs/rl
+
+echo "Setting EARLMCP environment variable to write to PyDOOCS"
+export EARLMCP=pydoocs
+
+echo "Runnig Python script"
+python evaluate_machine_agents.py
