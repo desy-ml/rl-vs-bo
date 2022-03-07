5603b2b46630ef61a4d0d980f11645ed789f9ce6
Jan Kaiser <jan.kaiser@desy.de>
2022-03-07 14:35:39 +0100

    Bugfix

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,5 +1,4 @@
 from .mse import ARESEAMSE
-from .onestep import ARESEAOneStep
 from .optimization import ARESEAOptimization
 from .sequential import ARESEASequential
 from .punish import ARESEAPunish
