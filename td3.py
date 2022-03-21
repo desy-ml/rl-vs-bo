c2576137b3b255a4101486ff6fb4f7852ef5a64e
Jan Kaiser <jan.kaiser@desy.de>
2022-03-21 17:36:50 +0100

    Simplified logging to W&B

--- td3.py
+++ td3.py
@@ -4,31 +4,15 @@ import glob
 from gym.wrappers import RescaleAction, TimeLimit
 import numpy as np
 from stable_baselines3 import TD3
-from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 import wandb
+from wandb.integration.sb3 import WandbCallback
 
 from environments import ARESEASequential, ResetActuators
 from utils import CheckpointCallback
 
-
-HYPERPARAMETER_DEFAULTS = {
-    "noise_type": "normal",
-    "noise_std": 0.1,
-    "learning_rate": 1e-3,
-    "buffer_size": 600000,
-    "learning_starts": 2000,
-    "batch_size": 100,
-    "tau": 0.005,
-    "gamma": 0.55,
-    "gradient_steps": -1,
-    "policy_delay": 2,
-    "target_policy_noise": 0.2,
-    "target_noise_clip": 0.5,
-    "net_arch": [64,32]
-}
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Train RL agent.")
@@ -51,34 +35,21 @@ def make_env():
 
 
 def setup_new_training():
-    wandb.config = HYPERPARAMETER_DEFAULTS
-
     env = DummyVecEnv([make_env])
-    env = VecNormalize(env, norm_obs=True, norm_reward=True, gamma=wandb.config["gamma"])
+    env = VecNormalize(env, norm_obs=True, norm_reward=True, gamma=0.55)
 
+    noise_std = 0.1
     n_actions = env.action_space.shape[-1]
-    if wandb.config["noise_type"] == "none":
-        noise = None
-    elif wandb.config["noise_type"] == "normal":
-        noise = NormalActionNoise(
-            mean=np.zeros(n_actions),
-            sigma=np.full(n_actions, wandb.config["noise_std"])
-        )
+    noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=np.full(n_actions, noise_std))
+
     model = TD3(
         "MlpPolicy",
         env,
         action_noise=noise,
-        learning_rate=wandb.config["learning_rate"],
-        buffer_size=wandb.config["buffer_size"],
-        learning_starts=wandb.config["learning_starts"],
-        batch_size=wandb.config["batch_size"],
-        tau=wandb.config["tau"],
-        gamma=wandb.config["gamma"],
-        gradient_steps=wandb.config["gradient_steps"],
-        policy_delay=wandb.config["policy_delay"],
-        target_policy_noise=wandb.config["target_policy_noise"],
-        target_noise_clip=wandb.config["target_noise_clip"],
-        policy_kwargs={"net_arch": wandb.config["net_arch"]},
+        buffer_size=600000,
+        learning_starts=2000,
+        gamma=0.55,
+        policy_kwargs={"net_arch": [64,32]},
         tensorboard_log=f"log/{wandb.run.name}",
         verbose=1,
         device="cpu"
@@ -122,7 +93,10 @@ def main():
 
     model = load_training(log_path) if wandb.run.resumed else setup_new_training()
 
-    callback = CheckpointCallback(3000, log_path, save_env=True, save_replay_buffer=True)
+    callbacks = [
+        CheckpointCallback(3000, log_path, save_env=True, save_replay_buffer=True),
+        WandbCallback(verbose=1)
+    ]
 
     eval_env = DummyVecEnv([make_env])
     eval_env = VecNormalize(eval_env, norm_obs=True, norm_reward=True, training=False)
@@ -130,7 +104,7 @@ def main():
     model.learn(
         total_timesteps=int(1e10),
         reset_num_timesteps=False,
-        callback=callback,
+        callback=callbacks,
         eval_env=eval_env,
         eval_freq=3000,
         tb_log_name="TD3"
