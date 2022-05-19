43974d34cb26843b9403d617d7f2ca98735b6a53
Jan Kaiser <jan.kaiser@desy.de>
2022-05-19 09:14:40 +0200

    Trainings to check if new environment works

---
+++ train_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --partition=maxgpu
+#SBATCH --job-name ares-ea-v2
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --constraint=A100|P100|V100
+#SBATCH --mail-type ALL
+
+source /etc/profile.d/modules.sh
+
+source ~/.bashrc
+conda activate rl39
+cd /beegfs/desy/user/kaiserja/ares-ea-v2
+
+python train.py --action_type direct --filter_action 0 1 3 --filter_observation beam magnets --incoming random --magnet_init zero --misalignments random --reward_method feedback --quad_action oneway --sb3_device cpu --vec_env dummy --w_mu_x 0.0 --w_mu_y 0.0 --w_time 0.0
+
+exit
