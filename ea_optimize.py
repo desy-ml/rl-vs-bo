d0220823bc06687a526bf6b6ff0e10f8f333856b
Jan Kaiser <jan.kaiser@desy.de>
2022-07-04 14:51:07 +0200

    Move machine application code to seperate file

---
+++ ea_optimize.py
@@ -0,0 +1,229 @@
+import numpy as np
+from gym.wrappers import FlattenObservation, RecordVideo, RescaleAction, TimeLimit
+from stable_baselines3 import TD3
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+
+from ea_train import ARESEA
+from utils import PolishedDonkeyCompatibility
+
+# import pydoocs
+import dummypydoocs as pydoocs
+
+
+def optimize(
+    target_mu_x,
+    target_sigma_x,
+    target_mu_y,
+    target_sigma_y,
+    threshold=3.3198e-6,
+    max_steps=50,
+    model_name="polished-donkey-996",
+    logbook=False,
+    callback=None,
+):
+    """
+    Function used for optimisation during operation.
+
+    Note: Current version only works for polished-donkey-996.
+    """
+    # config = read_from_yaml(f"models/{model}/config")
+    assert model_name == "polished-donkey-996", "Current version only works for polished-donkey-996."
+    
+    # Load the model
+    model = TD3.load(f"models/{model_name}/model")
+    
+    # Create the environment
+    def make_env_polished():
+        env = ARESEADOOCS(
+            dummy=False,
+            action_mode="delta",
+            magnet_init_mode="constant",
+            magnet_init_values=np.array([10, -10, 0, 10, 0]),
+            reward_mode="differential",
+            target_beam_mode="constant",
+            target_beam_values=np.array([target_mu_x, target_sigma_x, target_mu_y, target_sigma_y]),
+            target_beam_threshold=3.3198e-6,
+        )
+        if max_steps is not None:
+            env = TimeLimit(env, max_episode_steps=max_steps)
+        env = FlattenObservation(env)
+        env = PolishedDonkeyCompatibility(env)
+        env = RescaleAction(env, -1, 1)
+        env = RecordVideo(env, "recordings_function_test")
+        
+        return env
+
+    env = DummyVecEnv([make_env_polished])
+    env = VecNormalize.load(f"models/{model_name}/vec_normalize.pkl", env)
+    env.training = False
+
+    # Actual optimisation
+    observation = env.reset()
+    done = False
+    while not done:
+        action, _ = model.predict(observation, deterministic=True)
+        observation, reward, done, info = env.step(action)
+    env.close()
+
+
+class ARESEADOOCS(ARESEA):
+    
+    def __init__(
+        self,
+        action_mode="direct",
+        magnet_init_mode="zero",
+        magnet_init_values=None,
+        reward_mode="differential",
+        target_beam_mode="random",
+        target_beam_values=None,
+        target_beam_threshold=3.3198e-6,
+        w_mu_x=1.0,
+        w_mu_y=1.0,
+        w_on_screen=1.0,
+        w_sigma_x=1.0,
+        w_sigma_y=1.0,
+        w_time=1.0,
+    ):
+        super().__init__(
+            action_mode,
+            magnet_init_mode,
+            magnet_init_values,
+            reward_mode,
+            target_beam_mode,
+            target_beam_values,
+            target_beam_threshold,
+            w_mu_x,
+            w_mu_y,
+            w_on_screen,
+            w_sigma_x,
+            w_sigma_y,
+            w_time
+        )
+
+    def is_beam_on_screen(self):
+        return True # TODO find better logic
+
+    def get_magnets(self):
+        return np.array([
+            pydoocs.read("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM1/STRENGTH.RBV")["data"],
+            pydoocs.read("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM2/STRENGTH.RBV")["data"],
+            pydoocs.read("SINBAD.MAGNETS/MAGNET.ML/AREAMCVM1/KICK_MRAD.RBV")["data"] / 1000,
+            pydoocs.read("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM3/STRENGTH.RBV")["data"],
+            pydoocs.read("SINBAD.MAGNETS/MAGNET.ML/AREAMCHM1/KICK_MRAD.RBV")["data"] / 1000
+        ])
+    
+    def set_magnets(self, magnets):
+        pydoocs.write("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM1/STRENGTH.SP", magnets[0])
+        pydoocs.write("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM2/STRENGTH.SP", magnets[1])
+        pydoocs.write("SINBAD.MAGNETS/MAGNET.ML/AREAMCVM1/KICK_MRAD.SP", magnets[2] * 1000)
+        pydoocs.write("SINBAD.MAGNETS/MAGNET.ML/AREAMQZM3/STRENGTH.SP", magnets[3])
+        pydoocs.write("SINBAD.MAGNETS/MAGNET.ML/AREAMCHM1/KICK_MRAD.SP", magnets[4] * 1000)
+
+        # Wait until magnets have reached their setpoints
+        
+        time.sleep(3.0) # Wait for magnets to realise they received a command
+
+        magnets = ["AREAMQZM1", "AREAMQZM2", "AREAMCVM1", "AREAMQZM3", "AREAMCHM1"]
+
+        are_busy = [True] * 5
+        are_ps_on = [True] * 5
+        while any(are_busy) or not all(are_ps_on):
+            are_busy = [pydoocs.read(f"SINBAD.MAGNETS/MAGNET.ML/{magnet}/BUSY")["data"] for magnet in magnets]
+            are_ps_on = [pydoocs.read(f"SINBAD.MAGNETS/MAGNET.ML/{magnet}/PS_ON")["data"] for magnet in magnets]
+
+    def update_accelerator(self):
+        self.beam_image = self.capture_clean_beam_image()
+
+    def get_beam_parameters(self):
+        img = self.get_beam_image()
+        pixel_size = self.get_pixel_size()
+
+        parameters = {}
+        for axis, direction in zip([0,1], ["x","y"]):
+            projection = img.sum(axis=axis)
+            minfiltered = minimum_filter1d(projection, size=5, mode="nearest")
+            filtered = uniform_filter1d(minfiltered, size=5, mode="nearest")
+
+            half_values, = np.where(filtered >= 0.5 * filtered.max())
+
+            if len(half_values) > 0:
+                fwhm_pixel = half_values[-1] - half_values[0]
+                center_pixel = half_values[0] + fwhm_pixel / 2
+            else:
+                fwhm_pixel = 42     # TODO figure out what to do with these
+                center_pixel = 42
+
+            parameters[f"mu_{direction}"] = (center_pixel - len(filtered) / 2) * pixel_size[axis]
+            parameters[f"sigma_{direction}"] = fwhm_pixel / 2.355 * pixel_size[axis]
+            
+        parameters["mu_y"] = -parameters["mu_y"]
+
+        return np.array([
+            parameters["mu_x"],
+            parameters["sigma_x"],
+            parameters["mu_y"],
+            parameters["sigma_y"]
+        ])
+
+    def get_beam_image(self):
+        return self.beam_image
+
+    def get_binning(self):
+        return np.array((
+            pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/BINNINGHORIZONTAL")["data"],
+            pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/BINNINGVERTICAL")["data"]
+        ))
+
+    def get_screen_resolution(self):
+        return np.array([
+            pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/WIDTH")["data"],
+            pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/HEIGHT")["data"]
+        ])
+    
+    def get_pixel_size(self):
+        return np.array([
+            abs(pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/X.POLY_SCALE")["data"][2]) / 1000,
+            abs(pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/Y.POLY_SCALE")["data"][2]) / 1000
+        ]) * self.get_binning()
+
+    def capture_clean_beam_image(self, average=5):
+        """
+        Capture a clean image of the beam from the screen using `average` images with beam on and
+        `average` images of the background and then removing the background.
+        
+        Saves the image to a property of the object.
+        """
+         # Laser off
+        self.set_cathode_laser(False)
+        background_images = self.capture_interval(n=average, dt=0.1)
+        median_background = np.median(background_images.astype("float64"), axis=0)
+
+        # Laser on
+        self.set_cathode_laser(True)
+        beam_images = self.capture_interval(n=average, dt=0.1)
+        median_beam = np.median(beam_images.astype("float64"), axis=0)
+
+        removed = (median_beam - median_background).clip(0, 2**16-1)
+        flipped = np.flipud(removed)
+        
+        return flipped
+    
+    def capture_interval(self, n, dt):
+        """Capture `n` images from the screen and wait `dt` seconds in between them."""
+        images = []
+        for _ in range(n):
+            images.append(self.capture_screen())
+            time.sleep(dt)
+        return np.array(images)
+    
+    def capture_screen(self):
+        """Capture and image from the screen."""
+        return pydoocs.read("SINBAD.DIAG/CAMERA/AR.EA.BSC.R.1/IMAGE_EXT_ZMQ")["data"]
+
+    def set_cathode_laser(self, setto):
+        """Sets the bool switch of the cathode laser event to `setto` and waits a second."""
+        address = "SINBAD.DIAG/TIMER.CENTRAL/MASTER/EVENT5"
+        bits = pydoocs.read(address)["data"]
+        bits[0] = 1 if setto else 0
+        pydoocs.write(address, bits)
+        time.sleep(1)
