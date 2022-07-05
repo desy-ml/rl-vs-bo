3b92fade2b855068e621fa32b0a2d311c95d576c
Jan Kaiser <jan.kaiser@desy.de>
2022-07-05 22:29:24 +0200

    Add logging to logbook

--- ea_optimize.py
+++ ea_optimize.py
@@ -1,13 +1,17 @@
+from datetime import datetime
+from io import BytesIO
 import time
 
-import numpy as np
+import gym
 from gym.wrappers import FlattenObservation, RecordVideo, RescaleAction, TimeLimit
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.ndimage import minimum_filter1d, uniform_filter1d
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 from stable_baselines3 import TD3
 
 from ea_train import ARESEA
-from utils import PolishedDonkeyCompatibility
+from utils import PolishedDonkeyCompatibility, send_to_elog
 
 # import pydoocs
 import dummypydoocs as pydoocs
@@ -18,10 +22,10 @@ def optimize(
     target_sigma_x,
     target_mu_y,
     target_sigma_y,
-    target_mu_x_threshold=1e-4,
-    target_mu_y_threshold=1e-4,
-    target_sigma_x_threshold=1e-4,
-    target_sigma_y_threshold=1e-4,
+    target_mu_x_threshold=3.3198e-6,
+    target_mu_y_threshold=3.3198e-6,
+    target_sigma_x_threshold=3.3198e-6,
+    target_sigma_y_threshold=3.3198e-6,
     max_steps=50,
     model_name="polished-donkey-996",
     logbook=False,
@@ -54,6 +58,7 @@ def optimize(
         )
         if max_steps is not None:
             env = TimeLimit(env, max_episode_steps=max_steps)
+        env = ARESEARecorder(env, logbook=logbook, model_name=model_name)
         env = FlattenObservation(env)
         env = PolishedDonkeyCompatibility(env)
         env = RescaleAction(env, -1, 1)
@@ -243,3 +248,215 @@ class ARESEADOOCS(ARESEA):
         bits[0] = 1 if setto else 0
         pydoocs.write(address, bits)
         time.sleep(1)
+
+
+class ARESEARecorder(gym.Wrapper):
+
+    def __init__(self, env, logbook=False, model_name="-", path=None,):
+        super().__init__(env)
+
+        self.logbook = logbook
+        self.model_name = model_name
+        self.path = path
+
+        self.has_previously_run = False
+
+    def reset(self):
+        self.finalize_previous_episode()
+
+        observation = super().reset()
+
+        self.observations = [observation]
+        self.rewards = []
+        self.infos = []
+        self.beam_images = [self.env.get_beam_image()]
+        self.t_start = datetime.now()
+        self.t_end = None
+        self.steps_taken = 0
+        self.has_previously_run = True
+
+        return observation
+    
+    def step(self, action):
+        observation, reward, done, info = super().step(action)
+
+        self.observations.append(observation)
+        self.rewards.append(reward)
+        self.infos.append(info)
+        self.beam_images.append(self.env.get_beam_image())
+        self.steps_taken += 1
+
+        return observation, reward, done, info
+    
+    def close(self):
+        self.finalize_previous_episode()
+        return super().close()
+    
+    def finalize_previous_episode(self):
+        if not self.has_previously_run:
+            return
+
+        self.t_end = datetime.now()
+
+        if self.path is not None:
+            self.write_to_file()
+        if self.logbook:
+            self.write_to_logbook()
+    
+    def write_to_file(self):
+        pass
+    
+    def write_to_logbook(self):
+        # Create text message
+        beam_before = self.observations[0]["beam"]
+        beam_after = self.observations[-1]["beam"]
+        final_magnets = self.observations[-1]["magnets"]
+        msg = f"""Reinforcement learning agent optimised beam on AREABSCR1
+        
+        Agent: {self.model_name}
+        No. of steps: {self.steps_taken}
+        Time taken: {self.t_end - self.t_start}
+
+        Beam before:
+            mu_x    = {beam_before[0] * 1e3} mm
+            sigma_x = {beam_before[1] * 1e3} mm
+            mu_y    = {beam_before[2] * 1e3} mm
+            sigma_y = {beam_before[3] * 1e3} mm
+
+        Beam after:
+            mu_x    = {beam_after[0] * 1e3} mm
+            sigma_x = {beam_after[1] * 1e3} mm
+            mu_y    = {beam_after[2] * 1e3} mm
+            sigma_y = {beam_after[3] * 1e3} mm
+
+        Final magnet settings:
+            AREAMQZM1 strength = {final_magnets[0]} 1/m
+            AREAMQZM2 strength = {final_magnets[1]} 1/m
+            AREAMCVM1 kick     = {final_magnets[2] * 1e3} mrad
+            AREAMQZM3 strength = {final_magnets[3]} 1/m
+            AREAMCHM1 kick     = {final_magnets[4] * 1e3} mrad
+        """
+
+        # Create plot as jpg
+        fig, axs = plt.subplots(1, 5, figsize=(30,4))
+        self.plot_quadrupole_history(axs[0])
+        self.plot_steerer_history(axs[1])
+        self.plot_beam_history(axs[2])
+        self.plot_beam_before(axs[3])
+        self.plot_beam_after(axs[4])
+        fig.tight_layout()
+        
+        buf = BytesIO()
+        fig.savefig(buf, dpi=300, format="jpg")
+        buf.seek(0)
+        img = bytes(buf.read())
+    
+        # Send to logbook
+        send_to_elog(
+            elog="areslog",
+            author="Autonomous ARES",
+            title="RL-based Beam Optimisation on AREABSCR1",
+            severity="None",
+            text=msg,
+            image=img,
+        )
+        # If logbook not reachable just show everything
+        
+        print(msg)
+
+    def plot_quadrupole_history(self, ax):
+        areamqzm1 = [obs["magnets"][0] for obs in self.observations]
+        areamqzm2 = [obs["magnets"][1] for obs in self.observations]
+        areamqzm3 = [obs["magnets"][3] for obs in self.observations]
+
+        steps = np.arange(self.steps_taken + 1)
+
+        ax.set_title("Quadrupoles")
+        ax.set_xlim([0, self.steps_taken+1])
+        ax.set_xlabel("Step")
+        ax.set_ylabel("Strength (1/m)")
+        ax.plot(steps, areamqzm1, label="AREAMQZM1")
+        ax.plot(steps, areamqzm2, label="AREAMQZM2")
+        ax.plot(steps, areamqzm3, label="AREAMQZM3")
+        ax.legend()
+        ax.grid(True)
+
+    def plot_steerer_history(self, ax):
+        areamcvm1 = np.array([obs["magnets"][2] for obs in self.observations])
+        areamchm2 = np.array([obs["magnets"][4] for obs in self.observations])
+
+        steps = np.arange(self.steps_taken + 1)
+
+        ax.set_title("Steerers")
+        ax.set_xlabel("Step")
+        ax.set_ylabel("Kick (mrad)")
+        ax.set_xlim([0, self.steps_taken+1])
+        ax.plot(steps, areamcvm1*1e3, label="AREAMCVM1")
+        ax.plot(steps, areamchm2*1e3, label="AREAMCHM2")
+        ax.legend()
+        ax.grid(True)
+
+    def plot_beam_history(self, ax):
+        mu_x = np.array([obs["beam"][0] for obs in self.observations])
+        sigma_x = np.array([obs["beam"][1] for obs in self.observations])
+        mu_y = np.array([obs["beam"][2] for obs in self.observations])
+        sigma_y = np.array([obs["beam"][3] for obs in self.observations])
+
+        target_beam = self.observations[0]["target"]
+
+        steps = np.arange(self.steps_taken + 1)
+
+        ax.set_title("Beam Parameters")
+        ax.set_xlim([0, self.steps_taken+1])
+        ax.set_xlabel("Step")
+        ax.set_ylabel("(mm)")
+        ax.plot(steps, mu_x*1e3, label=r"$\mu_x$", c="tab:blue")
+        ax.plot(steps, [target_beam[0]*1e3]*len(steps), ls="--", c="tab:blue")
+        ax.plot(steps, sigma_x*1e3, label=r"$\sigma_x$", c="tab:orange")
+        ax.plot(steps, [target_beam[1]*1e3]*len(steps), ls="--", c="tab:orange")
+        ax.plot(steps, mu_y*1e3, label=r"$\mu_y$", c="tab:green")
+        ax.plot(steps, [target_beam[2]*1e3]*len(steps), ls="--", c="tab:green")
+        ax.plot(steps, sigma_y*1e3, label=r"$\sigma_y$", c="tab:red")
+        ax.plot(steps, [target_beam[3]*1e3]*len(steps), ls="--", c="tab:red")
+        ax.legend()
+        ax.grid(True)
+     
+    def plot_beam_before(self, ax):
+        img = self.beam_images[0]
+        screen_size = self.env.get_screen_resolution() * self.env.get_pixel_size()
+
+        ax.set_title("Beam Before (Background Removed)")
+        ax.set_xlabel("(mm)")
+        ax.set_ylabel("(mm)")
+        ax.imshow(
+            img,
+            vmin=0,
+            aspect="equal",
+            interpolation="none",
+            extent=(
+                -screen_size[0] / 2 * 1e3,
+                screen_size[0] / 2 * 1e3,
+                -screen_size[1] / 2 * 1e3,
+                screen_size[1] / 2 * 1e3,
+            ),
+        )
+
+    def plot_beam_after(self, ax):
+        img = self.beam_images[-1]
+        screen_size = self.env.get_screen_resolution() * self.env.get_pixel_size()
+
+        ax.set_title("Beam After (Background Removed)")
+        ax.set_xlabel("(mm)")
+        ax.set_ylabel("(mm)")
+        ax.imshow(
+            img,
+            vmin=0,
+            aspect="equal",
+            interpolation="none",
+            extent=(
+                -screen_size[0] / 2 * 1e3,
+                screen_size[0] / 2 * 1e3,
+                -screen_size[1] / 2 * 1e3,
+                screen_size[1] / 2 * 1e3,
+            ),
+        )
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
+import base64
 from datetime import datetime, timedelta
 import os
+import subprocess
 
 import gym
 from gym import spaces
@@ -237,3 +239,53 @@ class PolishedDonkeyCompatibility(gym.Wrapper):
             action[2],
             action[4],
         ])
+
+
+def send_to_elog(author, title, severity, text, elog, image=None):
+    """Send information to a supplied electronic logbook."""
+
+    # The DOOCS elog expects an XML string in a particular format. This string
+    # is beeing generated in the following as an initial list of strings.
+    succeded = True  # indicator for a completely successful job
+    # list beginning
+    elogXMLStringList = ['<?xml version="1.0" encoding="ISO-8859-1"?>',
+                         '<entry>']
+    # author information
+    elogXMLStringList.append('<author>')
+    elogXMLStringList.append(author)
+    elogXMLStringList.append('</author>')
+    # title information
+    elogXMLStringList.append('<title>')
+    elogXMLStringList.append(title)
+    elogXMLStringList.append('</title>')
+    # severity information
+    elogXMLStringList.append('<severity>')
+    elogXMLStringList.append(severity)
+    elogXMLStringList.append('</severity>')
+    # text information
+    elogXMLStringList.append('<text>')
+    elogXMLStringList.append(text)
+    elogXMLStringList.append('</text>')
+    # image information
+    if image:
+        try:
+            encodedImage = base64.b64encode(image)
+            elogXMLStringList.append('<image>')
+            elogXMLStringList.append(encodedImage.decode())
+            elogXMLStringList.append('</image>')
+        except:  # make elog entry anyway, but return error (succeded = False)
+            succeded = False
+    # list end
+    elogXMLStringList.append('</entry>')
+    # join list to the final string
+    elogXMLString = '\n'.join(elogXMLStringList)
+    # open printer process
+    try:
+        lpr =  subprocess.Popen(['/usr/bin/lp', '-o', 'raw', '-d', elog],
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        # send printer job
+        lpr.communicate(elogXMLString.encode('utf-8'))
+    except Exception as e:
+        print(e)
+        succeded = False
+    return succeded
