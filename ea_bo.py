d058ed9c155d2cf637a8c4311548cd4a2415437a
Jan Kaiser <jan.kaiser@desy.de>
2022-09-27 22:03:06 +0200

    Move BO optimization method to seperate file

---
+++ ea_bo.py
@@ -0,0 +1,207 @@
+from datetime import datetime
+
+import numpy as np
+import torch
+from gym.wrappers import (
+    FilterObservation,
+    FlattenObservation,
+    FrameStack,
+    RecordVideo,
+    RescaleAction,
+    TimeLimit,
+)
+from stable_baselines3.common.env_util import unwrap_wrapper
+
+from bayesopt import calculate_objective, get_new_bound, get_next_samples, scale_action
+from ea_optimize import (
+    ARESEADOOCS,
+    ARESEAeLog,
+    BaseCallback,
+    CallbackList,
+    OptimizeFunctionCallback,
+    TQDMWrapper,
+    setup_callback,
+)
+from utils import (
+    FilterAction,
+    NotVecNormalize,
+    PolishedDonkeyCompatibility,
+    RecordEpisode,
+    send_to_elog,
+)
+
+config = config = {
+    "action_mode": "direct_unidirectional_quads",
+    "gamma": 0.99,
+    # "filter_action": [0, 1, 3],
+    "filter_action": None,
+    "filter_observation": None,
+    "frame_stack": None,
+    "incoming_mode": "random",
+    "incoming_values": None,
+    "magnet_init_mode": "constant",
+    "magnet_init_values": np.array([10, -10, 0, 10, 0]),
+    "misalignment_mode": "constant",
+    "misalignment_values": np.zeros(8),
+    "n_envs": 40,
+    "normalize_observation": True,
+    "normalize_reward": True,
+    "rescale_action": (-3, 3),
+    "reward_mode": "logl1",
+    "sb3_device": "auto",
+    "target_beam_mode": "constant",
+    "target_beam_values": np.zeros(4),
+    "target_mu_x_threshold": 1e-5,
+    "target_mu_y_threshold": 1e-5,
+    "target_sigma_x_threshold": 1e-5,
+    "target_sigma_y_threshold": 1e-5,
+    "threshold_hold": 5,
+    "time_limit": 50000,
+    "vec_env": "subproc",
+    "w_done": 0.0,
+    "w_mu_x": 1.0,
+    "w_mu_x_in_threshold": 0.0,
+    "w_mu_y": 1.0,
+    "w_mu_y_in_threshold": 0.0,
+    "w_on_screen": 100.0,
+    "w_sigma_x": 1.0,
+    "w_sigma_x_in_threshold": 0.0,
+    "w_sigma_y": 1.0,
+    "w_sigma_y_in_threshold": 0.0,
+    "w_time": 0.0,
+}
+
+
+# define a similar optimize function as in ea_optimize.py
+
+
+def optimize(
+    target_mu_x,
+    target_sigma_x,
+    target_mu_y,
+    target_sigma_y,
+    target_mu_x_threshold=3.3198e-6,
+    target_mu_y_threshold=3.3198e-6,
+    target_sigma_x_threshold=3.3198e-6,
+    target_sigma_y_threshold=3.3198e-6,
+    max_steps=100,
+    model_name="BO",
+    logbook=False,
+    data_log_dir=None,
+    progress_bar=False,
+    callback=BaseCallback(),
+    stepsize=0.1,  # comparable to RL env
+    obj_function="logmae",
+    acquisition="EI",
+    init_x=None,
+    init_samples=5,
+    filter_action=None,
+    set_to_best=True,  # set back to best found setting after opt.
+):
+    callback = setup_callback(callback)
+
+    # Create the environment
+    env = ARESEADOOCS(
+        action_mode=config["action_mode"],
+        magnet_init_mode=config["magnet_init_mode"],
+        magnet_init_values=config["magnet_init_values"],
+        reward_mode=config["reward_mode"],
+        target_beam_mode=config["target_beam_mode"],
+        target_beam_values=np.array(
+            [target_mu_x, target_sigma_x, target_mu_y, target_sigma_y]
+        ),
+        target_mu_x_threshold=target_mu_x_threshold,
+        target_mu_y_threshold=target_mu_y_threshold,
+        target_sigma_x_threshold=target_sigma_x_threshold,
+        target_sigma_y_threshold=target_sigma_y_threshold,
+        threshold_hold=1,
+        w_done=config["w_done"],
+        w_mu_x=config["w_mu_x"],
+        w_mu_x_in_threshold=config["w_mu_x_in_threshold"],
+        w_mu_y=config["w_mu_y"],
+        w_mu_y_in_threshold=config["w_mu_y_in_threshold"],
+        w_on_screen=config["w_on_screen"],
+        w_sigma_x=config["w_sigma_x"],
+        w_sigma_x_in_threshold=config["w_sigma_x_in_threshold"],
+        w_sigma_y=config["w_sigma_y"],
+        w_sigma_y_in_threshold=config["w_sigma_y_in_threshold"],
+        w_time=config["w_time"],
+    )
+    if max_steps is not None:
+        env = TimeLimit(env, max_steps)
+    if progress_bar:
+        env = TQDMWrapper(env)
+    if callback is not None:
+        env = OptimizeFunctionCallback(env, callback)
+    if data_log_dir is not None:
+        env = RecordEpisode(env, save_dir=data_log_dir)
+    if logbook:
+        env = ARESEAeLog(env, model_name=model_name)
+    if config["filter_observation"] is not None:
+        env = FilterObservation(env, config["filter_observation"])
+    if config["filter_action"] is not None:
+        env = FilterAction(env, config["filter_action"], replace=0)
+    env = FlattenObservation(env)
+    if config["frame_stack"] is not None:
+        env = FrameStack(env, config["frame_stack"])
+    if config["rescale_action"] is not None:
+        env = RescaleAction(
+            env, config["rescale_action"][0], config["rescale_action"][1]
+        )
+    env = RecordVideo(env, video_folder=f"recordings_real/{datetime.now():%Y%m%d%H%M}")
+    # env = NotVecNormalize(env, f"models/{model_name}/vec_normalize.pkl")
+
+    callback.env = env
+
+    # Actual optimisation
+    observation = env.reset()
+    done = False
+
+    # Initialization
+    x_dim = env.action_space.shape[0]
+    # bounds = torch.tensor(
+    #     np.array([env.action_space.low, env.action_space.high]), dtype=torch.float32
+    # )
+    if init_x is not None:  # From fix starting points
+        X = torch.tensor(init_x.reshape(-1, x_dim), dtype=torch.float32)
+    else:  # Random Initialization-5.7934
+        action_i = scale_action(env, observation, filter_action)
+        X = torch.tensor([action_i], dtype=torch.float32)
+        bounds = get_new_bound(env, action_i, stepsize)
+        for i in range(init_samples - 1):
+            new_action = np.random.uniform(low=bounds[0], high=bounds[1])
+            X = torch.cat([X, torch.tensor([new_action])])
+    # Sample initial Ys to build GP
+    Y = torch.empty((X.shape[0], 1))
+    for i, action in enumerate(X):
+        action = action.detach().numpy()
+        observation, reward, done, info = env.step(action)
+        objective = calculate_objective(env, observation, reward, obj=obj_function)
+        Y[i] = torch.tensor(objective)
+
+    # Actual BO Loop
+    while not done:
+        current_action = X[-1].detach().numpy()
+        bounds = get_new_bound(env, current_action, stepsize)
+        action_t = get_next_samples(
+            X,
+            Y,
+            Y.max(),
+            torch.tensor(bounds, dtype=torch.float32),
+            n_points=1,
+            acquisition=acquisition,
+        )
+        action = action_t.detach().numpy().flatten()
+        observation, reward, done, info = env.step(action)
+        objective = calculate_objective(env, observation, reward, obj=obj_function)
+
+        # append data
+        X = torch.cat([X, action_t])
+        Y = torch.cat([Y, torch.tensor([[objective]], dtype=torch.float32)])
+
+    # Set back to
+    if set_to_best:
+        action = X[Y.argmax()].detach().numpy()
+        observation, reward, done, info = env.step(action)
+
+    env.close()
