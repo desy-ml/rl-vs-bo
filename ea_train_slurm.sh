db8b79d99f8b6a66770cb3020265425323097cce
Jan Kaiser <jan.kaiser@desy.de>
2022-11-25 10:07:15 +0100

    Hyperparameter exploration for max delta actions

--- ea_train_slurm.sh
+++ ea_train_slurm.sh
@@ -12,6 +12,7 @@ source ~/.bashrc
 conda activate rl39
 cd /beegfs/desy/user/kaiserja/ares-ea-v2
 
-python ea_train.py
+# python ea_train.py
+wandb agent --count 1 msk-ipc/ares-ea-v2/gun6p5gd
 
 exit
