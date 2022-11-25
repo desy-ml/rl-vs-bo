80bceda4e7c28a92f25f3424fdb20c5111365c47
Jan Kaiser <jan.kaiser@desy.de>
2022-11-25 10:08:00 +0100

    From evaluating BO with NN prior

---
+++ eval_bo_nnprior.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --partition=maxgpu
+#SBATCH --job-name ares-ea-v2
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --constraint=P100|V100
+#SBATCH --mail-type ALL
+
+source /etc/profile.d/modules.sh
+
+source ~/.bashrc
+conda activate rl39
+cd /beegfs/desy/user/kaiserja/ares-ea-v2
+
+python eval_bo_nnprior.py
+
+exit
