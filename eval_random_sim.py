85c2d9b5ba1955a82ed7448f34e792a6c69fb3f8
Jan Kaiser <jan.kaiser@desy.de>
2022-12-14 08:17:49 +0100

    Evaluate random optimisation and find worst real RL problem

---
+++ eval_random_sim.py
@@ -0,0 +1,68 @@
+import json
+from concurrent.futures import ProcessPoolExecutor
+
+import numpy as np
+from gym.wrappers import FilterObservation, FlattenObservation, RescaleAction, TimeLimit
+from stable_baselines3 import TD3
+from tqdm.notebook import tqdm
+
+from ea_train import ARESEACheetah
+from eval_bo_sim import (
+    convert_incoming_from_problem,
+    convert_misalignments_from_problem,
+    convert_target_from_problem,
+)
+from utils import RecordEpisode
+
+
+def try_problem(problem_index: dict, problem: int) -> None:
+    model_name = "polished-donkey-996"
+
+    # Load the model
+    model = TD3.load(f"models/{model_name}/model")
+
+    # Create the environment
+    env = ARESEACheetah(
+        action_mode="direct",
+        incoming_mode="constant",
+        incoming_values=convert_incoming_from_problem(problem),
+        magnet_init_mode="constant",
+        magnet_init_values=np.array([10, -10, 0, 10, 0]),
+        misalignment_mode="constant",
+        misalignment_values=convert_misalignments_from_problem(problem),
+        reward_mode="differential",
+        target_beam_mode="constant",
+        target_beam_values=convert_target_from_problem(problem),
+        target_mu_x_threshold=None,
+        target_mu_y_threshold=None,
+        target_sigma_x_threshold=None,
+        target_sigma_y_threshold=None,
+        threshold_hold=5,
+    )
+    env = TimeLimit(env, 150)
+    env = RecordEpisode(
+        env, save_dir=f"data/bo_vs_rl/simulation/random/problem_{problem_index:03d}"
+    )
+    env = FilterObservation(env, ["beam", "magnets", "target"])
+    env = FlattenObservation(env)
+    env = RescaleAction(env, -1, 1)
+
+    # Actual optimisation
+    observation = env.reset()
+    done = False
+    while not done:
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+    env.close()
+
+
+def main():
+    with open("problems.json", "r") as f:
+        problems = json.load(f)
+
+    with ProcessPoolExecutor() as executor:
+        _ = tqdm(executor.map(try_problem, range(len(problems)), problems), total=300)
+
+
+if __name__ == "__main__":
+    main()
