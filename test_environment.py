39d2bca9699838a79bcfbeffe6061384722c3332
Jan Kaiser <jan.kaiser@desy.de>
2023-02-01 16:59:01 +0100

    Tests for desired transverse tuning environments

--- test_environment.py
+++ test_environment.py
@@ -2,14 +2,41 @@ import pytest
 from gym.wrappers import RescaleAction
 from stable_baselines3.common.env_checker import check_env
 
-from backend import EACheetahBackend, EADOOCSBackend, EAOcelotBackend
-from environment import EATransverseTuning
+from backend import (
+    BCCheetahBackend,
+    BCDOOCSBackend,
+    EACheetahBackend,
+    EADOOCSBackend,
+    EAOcelotBackend,
+)
+from environment import EATransverseTuning, MRTransverseTuning
 
 
 @pytest.mark.parametrize(
     "backend_cls", [EACheetahBackend, EADOOCSBackend, EAOcelotBackend]
 )
-def test_check_env(backend_cls):
+def test_ea_check_env(backend_cls):
     env = EATransverseTuning(backend=backend_cls(), w_beam=1.0, w_mu_x=1.0)
+    env = RescaleAction(env, -1, 1)
+    check_env(env)
+
+
+@pytest.mark.parametrize("backend_cls", [BCCheetahBackend, BCDOOCSBackend])
+def test_bc_check_env(backend_cls):
+    env = MRTransverseTuning(backend=backend_cls(), w_beam=1.0, w_mu_x=1.0)
+    env = RescaleAction(env, -1, 1)
+    check_env(env)
+
+
+@pytest.mark.parametrize("backend_cls", [DLCheetahBackend, DLDOOCSBackend])
+def test_dl_check_env(backend_cls):
+    env = DLTransverseTuning(backend=backend_cls(), w_beam=1.0, w_mu_x=1.0)
+    env = RescaleAction(env, -1, 1)
+    check_env(env)
+
+
+@pytest.mark.parametrize("backend_cls", [SHCheetahBackend, SHDOOCSBackend])
+def test_sh_check_env(backend_cls):
+    env = SHTransverseTuning(backend=backend_cls(), w_beam=1.0, w_mu_x=1.0)
     env = RescaleAction(env, -1, 1)
     check_env(env)
