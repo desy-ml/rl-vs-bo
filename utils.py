22468a8cc9ba265973db6d9af87c533fe36acf0a
Jan Kaiser <jan.kaiser@desy.de>
2023-02-26 17:02:39 +0100

    Evaluate magnet failure for RL and BO

--- utils.py
+++ utils.py
@@ -939,3 +939,45 @@ class AnimateIncomingBeam(gym.Wrapper):
             sigma_s=new_beam_parameters[9],
             sigma_p=new_beam_parameters[10],
         )
+
+
+class FailQ3(gym.Wrapper):
+    """Turn magnet Q3 off as if it had failed."""
+
+    def __init__(self, env: gym.Env, at_step: int = 0) -> None:
+        super().__init__(env)
+
+        assert isinstance(env.unwrapped.backend, CheetahBackend)
+
+        self.at_step = at_step
+
+    def reset(self) -> Union[np.ndarray, dict]:
+        obs = super().reset()
+
+        self.steps_taken = 0
+        self.has_magnet_failed = False
+
+        if self.at_step == 0:
+            magnet_values = self.env.unwrapped.backend.get_magnets()
+            magnet_values[3] = 0.0
+            self.env.unwrapped.backend.set_magnets(magnet_values)
+
+            self.has_magnet_failed = True
+
+            obs["magnets"][3] = 0.0
+
+        return obs
+
+    def step(self, action: np.ndarray) -> tuple:
+        self.steps_taken += 1
+
+        if self.steps_taken > self.at_step:
+            magnet_values = self.env.unwrapped.backend.get_magnets()
+            magnet_values[3] = 0.0
+            self.env.unwrapped.backend.set_magnets(magnet_values)
+
+            self.has_magnet_failed = True
+
+            action[3] = 0.0
+
+        return super().step(action)
