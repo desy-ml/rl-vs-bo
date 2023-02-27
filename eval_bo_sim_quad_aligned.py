0dd8e616e23ead4642924fe16f6624053d5e1864
Jan Kaiser <jan.kaiser@desy.de>
2023-02-27 10:08:08 +0100

    First implementation of beam-to-quad aligned evaluation in simulation (failing tests)

---
+++ eval_bo_sim_quad_aligned.py
@@ -0,0 +1,114 @@
+from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
+
+import numpy as np
+from gym.wrappers import RescaleAction, TimeLimit
+from scipy.stats import linregress
+from tqdm import tqdm
+
+from backend import EACheetahBackend
+from bayesopt import BayesianOptimizationAgent
+from environment import EATransverseTuning
+from trial import Trial, load_trials
+from utils import RecordEpisode
+
+
+def try_problem(trial_index: int, trial: Trial):
+    # Create the environment
+    cheetah_backend = EACheetahBackend(
+        incoming_mode="constant",
+        incoming_values=trial.incoming_beam,
+        misalignment_mode="constant",
+        misalignment_values=trial.misalignments,
+    )
+    env = EATransverseTuning(
+        backend=cheetah_backend,
+        action_mode="direct",
+        magnet_init_mode="constant",
+        magnet_init_values=np.array([10, -10, 0, 10, 0]),
+        reward_mode="feedback",
+        target_beam_mode="constant",
+        target_beam_values=trial.target_beam,
+        target_mu_x_threshold=None,
+        target_mu_y_threshold=None,
+        target_sigma_x_threshold=None,
+        target_sigma_y_threshold=None,
+        threshold_hold=5,
+        unidirectional_quads=True,
+        w_beam=1.0,
+        w_mu_x=1.0,
+        w_mu_y=1.0,
+        w_on_screen=10.0,
+        w_sigma_x=1.0,
+        w_sigma_y=1.0,
+        logarithmic_beam_distance=True,
+        normalize_beam_distance=False,
+    )
+    env = TimeLimit(env, 150)
+    env = RecordEpisode(
+        env,
+        save_dir=f"data/bo_vs_rl/simulation/bo_quad_aligned/problem_{trial_index:03d}",
+    )
+    env = RescaleAction(env, -3, 3)
+
+    model = BayesianOptimizationAgent(
+        env=env,
+        stepsize=0.1,
+        init_samples=5,
+        acquisition="EI",
+        mean_module=None,
+    )
+
+    # Actual optimisation
+    observation = env.reset()
+    reward = None
+    done = False
+    while not done:
+        action = model.predict(observation, reward)
+        observation, reward, done, info = env.step(action)
+
+    # Set back to best
+    action = model.X[model.Y.argmax()].detach().numpy()
+    env.step(action)
+
+    env.close()
+
+
+def find_quad_aligned_incoming_beam_parameters(trial: Trial) -> np.ndarray:
+    """
+    Return incoming beam parameters that aligned the beam as best as possible to the
+    misaligned quadrupoles.
+    """
+    s = [0.24, 0.79, 1.33]  # Hardcoded s-positions of centers of the quadrupoles
+    x = trial.misalignments[[0, 2, 4]]
+    y = trial.misalignments[[1, 3, 5]]
+
+    x_fit = linregress(s, x)
+    y_fit = linregress(s, y)
+
+    print(f"{x_fit = }")
+    print(f"{y_fit = }")
+
+    incoming_beam = trial.incoming_beam.copy()
+    incoming_beam[1] = x_fit.intercept
+    incoming_beam[2] = np.arctan(x_fit.slope)
+    incoming_beam[3] = y_fit.intercept
+    incoming_beam[4] = np.arctan(y_fit.slope)
+
+    print(f"{incoming_beam = }")
+
+    return incoming_beam
+
+
+def main():
+    trials = load_trials(Path("trials.yaml"))
+
+    for trial in trials:
+        trial.incoming_beam = find_quad_aligned_incoming_beam_parameters(trial)
+
+    with ProcessPoolExecutor() as executor:
+        _ = tqdm(executor.map(try_problem, range(len(trials)), trials), total=300)
+
+
+if __name__ == "__main__":
+    main()
--- test_environment.py
+++ test_environment.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import cheetah
 import pytest
 from gym.wrappers import RescaleAction
 from stable_baselines3.common.env_checker import check_env
@@ -19,6 +22,8 @@ from environment import (
     EATransverseTuning,
     SHTransverseTuning,
 )
+from eval_bo_sim_quad_aligned import find_quad_aligned_incoming_beam_parameters
+from trial import load_trials
 
 
 @pytest.mark.parametrize(
@@ -49,3 +54,91 @@ def test_sh_check_env(backend_cls):
     env = SHTransverseTuning(backend=backend_cls(), w_beam=1.0, w_mu_x=1.0)
     env = RescaleAction(env, -1, 1)
     check_env(env)
+
+
+@pytest.mark.parametrize("trial_index", range(300))
+def test_simulation_beam_to_quadrupole_alignment(trial_index):
+    trial = load_trials(Path("trials.yaml"))[trial_index]
+    backend = EACheetahBackend(
+        misalignment_mode="constant", misalignment_values=trial.misalignments
+    )
+    backend.reset()
+    segment = backend.segment
+
+    bad_beam_parameters = trial.incoming_beam
+    bad_incoming_beam = cheetah.ParameterBeam.from_parameters(
+        energy=bad_beam_parameters[0],
+        mu_x=bad_beam_parameters[1],
+        mu_xp=bad_beam_parameters[2],
+        mu_y=bad_beam_parameters[3],
+        mu_yp=bad_beam_parameters[4],
+        sigma_x=bad_beam_parameters[5],
+        sigma_xp=bad_beam_parameters[6],
+        sigma_y=bad_beam_parameters[7],
+        sigma_yp=bad_beam_parameters[8],
+        sigma_s=bad_beam_parameters[9],
+        sigma_p=bad_beam_parameters[10],
+    )
+    good_beam_parameters = find_quad_aligned_incoming_beam_parameters(trial)
+    good_incoming_beam = cheetah.ParameterBeam.from_parameters(
+        energy=good_beam_parameters[0],
+        mu_x=good_beam_parameters[1],
+        mu_xp=good_beam_parameters[2],
+        mu_y=good_beam_parameters[3],
+        mu_yp=good_beam_parameters[4],
+        sigma_x=good_beam_parameters[5],
+        sigma_xp=good_beam_parameters[6],
+        sigma_y=good_beam_parameters[7],
+        sigma_yp=good_beam_parameters[8],
+        sigma_s=good_beam_parameters[9],
+        sigma_p=good_beam_parameters[10],
+    )
+
+    segment.AREAMQZM1.k1 = 0.0
+    segment.AREAMQZM2.k1 = 0.0
+    segment.AREAMQZM3.k1 = 0.0
+    segment.AREABSCR1.is_active = False
+
+    # Before beam-to-quad alignment
+    beam_0 = segment(bad_incoming_beam)
+    segment.AREAMQZM1.k1 = 10.0
+    beam_1 = segment(bad_incoming_beam)
+    segment.AREAMQZM1.k1 = 0.0
+    segment.AREAMQZM2.k1 = 10.0
+    beam_2 = segment(bad_incoming_beam)
+    segment.AREAMQZM2.k1 = 0.0
+    segment.AREAMQZM3.k1 = 10.0
+    beam_3 = segment(bad_incoming_beam)
+    segment.AREAMQZM3.k1 = 0.0
+
+    bad_movement = (
+        abs(beam_1.mu_x - beam_0.mu_x)
+        + abs(beam_2.mu_x - beam_0.mu_x)
+        + abs(beam_3.mu_x - beam_0.mu_x)
+        + abs(beam_1.mu_y - beam_0.mu_y)
+        + abs(beam_2.mu_y - beam_0.mu_y)
+        + abs(beam_3.mu_y - beam_0.mu_y)
+    )
+
+    # After beam-to-quad alignment
+    beam_0 = segment(good_incoming_beam)
+    segment.AREAMQZM1.k1 = 10.0
+    beam_1 = segment(good_incoming_beam)
+    segment.AREAMQZM1.k1 = 0.0
+    segment.AREAMQZM2.k1 = 10.0
+    beam_2 = segment(good_incoming_beam)
+    segment.AREAMQZM2.k1 = 0.0
+    segment.AREAMQZM3.k1 = 10.0
+    beam_3 = segment(good_incoming_beam)
+    segment.AREAMQZM3.k1 = 0.0
+
+    good_movement = (
+        abs(beam_1.mu_x - beam_0.mu_x)
+        + abs(beam_2.mu_x - beam_0.mu_x)
+        + abs(beam_3.mu_x - beam_0.mu_x)
+        + abs(beam_1.mu_y - beam_0.mu_y)
+        + abs(beam_2.mu_y - beam_0.mu_y)
+        + abs(beam_3.mu_y - beam_0.mu_y)
+    )
+
+    assert good_movement <= bad_movement
