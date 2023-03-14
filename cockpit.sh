e9d1beeb726fe4cb730d088abf3c7ea0dd4da2f2
Chenran Xu <chenran.xu@kit.edu>
2023-03-14 14:37:23 +0100

    Clean up not relevant code

--- cockpit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-# Script for running the RL Agent EA app from the ARES cockpit
-
-. /home/aresoper/.profile
-conda activate /home/kaiserja/.conda/envs/rl39ng
-cd /home/aresoper/user/kaiserja/ares-ea-v2
-python app.py
